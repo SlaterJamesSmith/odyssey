811c2d3c201c593b3ff1d650c41201054e6c3a7c
Student <example@example.com>
2019-03-06 15:38:05 -0800

    adjust annimation and remove scroll wheel speed

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,7 +1,39 @@
 import $ from "jquery";
 import WOW from 'wow.js';
 
+// function wheel(event) {
+//     var delta = 0;
+//     if (event.wheelDelta) {(delta = event.wheelDelta / 120);}
+//     else if (event.detail) {(delta = -event.detail / 3);}
+//
+//     handle(delta);
+//     if (event.preventDefault) {(event.preventDefault());}
+//     event.returnValue = false;
+// }
+//
+// function handle(delta) {
+//     var time = 2000;
+//     var distance = 25;
+//
+//     $('html, body').stop().animate({
+//         scrollTop: $(window).scrollTop() - (distance * delta)
+//     }, time );
+// }
+//
+// if (window.addEventListener) {window.addEventListener('DOMMouseScroll', wheel, false);}
+//   window.onmousewheel = document.onmousewheel = wheel;
+
 $(document).ready(function() {
+  $(".g-button").click(function(event) {
+    event.preventDefault();
+    $(".greek").show();
+    $(".english").fadeOut();
+  });
+  $(".e-button").click(function(event) {
+    event.preventDefault();
+    $(".english").show();
+    $(".greek").fadeOut();
+  });
   var wow = new WOW(
     {
       boxClass:     'wow',
@@ -33,21 +65,4 @@ $(document).ready(function() {
 
 
 
-
-
-
-  // window.addEventListener("load", function(){
-  //   var nodes = document.querySelectorAll(".story2");
-  //   for(var i=0; i<nodes.length; i++){
-  //     var words = nodes[i].innerText;
-  //     var html = "";
-  //     for(var i2=0; i2<words.length; i2++){
-  //       if(words[i2] == " ")html +=
-  //     words[i2];
-  //         else html +=
-  //     "<span>"+words[i2]+"</span>"
-  //     }
-  //     nodes[i].innerHTML = html;
-  //   }
-  // });
 });
